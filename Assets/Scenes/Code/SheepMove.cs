4fdabc74298a5dfd2dc64ce5f6faa6b554c15228
BLLivingstone <78822389+BLLivingstone@users.noreply.github.com>
2023-03-01 15:05:22 +0000

    NavMesh implementation

---
+++ Assets/Scenes/Code/SheepMove.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SheepMove : MonoBehaviour{
+    [SerializeField]
+    public Transform enemy;
+    public NavMeshAgent sheep;
+    public LayerMask ground, wolf;
+
+    //Wandering
+    public Vector3 movement;
+    public bool setMovement;
+    public float movementArea;
+
+    //while scared
+    public float timeResting;
+    public bool currentlyLooking;
+
+    //avoiding wolf
+    public float runArea, FOV;
+    public bool wolfInFOV, wolfInRunArea;
+    
+   //l "myWolfCharacter"
+
+/*   // Start is called before the first frame update
+    void Start() {
+        sheep = this.GetComponent<NavMeshAgent>();
+        if(sheep == null){
+            Debug.LogError("Error 404, please attach "+ gameObject.name);
+        }
+        else {
+            GoToNextPoint();
+        }
+    }
+    private void GoToNextPoint(){
+        if(target != null){
+            Vector3 vector = target.transform.position;
+            sheep.SetDestination(vector);
+        }
+    }
+*/  private void Alive(){
+        enemy = gameObject.Find("myWolfCharacter").transform;
+        sheep = GetComponent<NavMeshAgent>();
+    }
+    private void Update(){
+        //update sight range 
+        wolfInFOV = Physics.CheckSphere(transform.position, FOV, wolf);
+        wolfInRunArea = Physics.CheckSphere(transform.position, runArea, wolf);
+
+        if(!wolfInFOV && !wolfInRunArea) Wandering();
+        if(wolfInFOV && wolfInRunArea) Running();
+        if(wolfInFOV && !wolfInRunArea) Looking();
+    }
+    private void Wandering(){
+        if(!setMovement) NextWanderingPoint();
+
+        if(setMovement){
+            sheep.SetDestination(movement);
+        }
+
+        Vector3 wanderToPoint = transform.position - movement;
+
+        if(wanderToPoint.magnitude <1f){
+            setMovement =false;
+        }
+    }
+    private void NextWanderingPoint(){
+        float randZ = Random.Range(-movementArea, movementArea);
+        float randX = Random.Range(-movementArea, movementArea);
+
+        movement = new Vector3(transform.position.x + randX, transform.position.y, transform.position.z + randZ);
+
+        if (Physics.Raycast(movement, -transform.up, 2f, ground)){
+            setMovement = true;
+        }
+    } 
+    private void Running(){
+        if (runArea < FOV){
+            Vector3 directionOfWolf = transform.position - enemy.transform.position;
+            movement = transform.position + directionOfWolf;
+
+            sheep.SetDestination(movement);
+        }
+    
+    }
+    private void Looking(){
+        sheep.SetDestination(transform.position);
+        transform.LookAt(enemy);
+
+        if(!currentlyLooking){
+            currentlyLooking = true;
+            Invoke(nameof(ResetLook), timeResting);
+        }
+    }
+    private void ResetLook(){
+        currentlyLooking = false;
+    }
+}
+
+
